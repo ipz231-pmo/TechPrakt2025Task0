127d0330a23c7c4dad743e364e628704b8da6cd9
Пеньков Максим Олександрович <ipz231_pmo@student.ztu.edu.ua>
2025-01-23 21:40:08 +0200

    Super useful program was written!

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿
+double num1, num2;
+
+while(true)
+{
+    Console.Write("Enter first number: ");
+    string? tmp = Console.ReadLine();
+    if (double.TryParse(tmp, out num1)) break;
+    Console.WriteLine("Error. Failed to parse string to number. Please try again.");
+}
+
+while (true)
+{
+    Console.Write("Enter second number: ");
+    string? tmp = Console.ReadLine();
+    if (double.TryParse(tmp, out num2)) break;
+    Console.WriteLine("Error. Failed to parse string to number. Please try again.");
+}
+
+
+Console.WriteLine($"Sum is {num1 + num2}");
